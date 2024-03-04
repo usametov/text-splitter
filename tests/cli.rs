29ccbfc0ebd9fcd8165850d919e72efdd9c2bc96
usametov <usametov@gmail.com>
2024-03-03 21:44:32 -0500

    remove -v option

--- tests/cli.rs
+++ tests/cli.rs
@@ -34,7 +34,7 @@ fn runs_with_strip() {
     cmd.args(["--input-files", &format!("/media/{}/ssd1/aiken/files2process.txt", current_user), 
                "--dir", &format!("/media/{}/ssd1/aiken/aiken-lang-docs/src/", current_user),
                "-o", &format!("/media/{}/ssd1/aiken/aiken-docs-processed/", current_user), 
-               "--minchar", "400", "--maxchar", "1200", "-v",
+               "--minchar", "400", "--maxchar", "1200",
                "--strip-prefix", &format!("/media/{}/ssd1/aiken/aiken-lang-docs/src/pages/", current_user),
                "--prefix-replace", "https://aiken-lang.org/"])
                .assert().success();
