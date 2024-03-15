72500e320fae4e5daf0fcebbc37a65e357c2b3a7
usametov <usametov@gmail.com>
2024-03-14 23:20:03 -0400

    clean up

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,44 +1,41 @@
-#[cfg(test)]
-mod tests {
+use assert_cmd::Command; 
+use predicates::prelude::*;
+use std::env;
 
-    use assert_cmd::Command; 
-    use predicates::prelude::*;
-    use std::env;
+#[test]
+fn dies_no_args() {
 
-    #[test]
-    fn dies_no_args() {
-
-        let mut cmd = Command::cargo_bin("text-splitter").unwrap();
-        cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("USAGE"));
-        
-    }
+    let mut cmd = Command::cargo_bin("text-splitter").unwrap();
+    cmd.assert()
+    .failure()
+    .stderr(predicate::str::contains("USAGE"));
+    
+}
 
 
-    #[test]
-    fn runs_nostrip() {
-        let mut cmd = Command::cargo_bin("text-splitter").unwrap();
-        cmd.args(["--input-files", "tests/inputs/files2process.txt", 
-                "--dir", "tests/inputs/", "-o", "tests/outputs/", 
-                "--minchar", "400", "--maxchar", "1200", "-v"]).assert().success();
-    }
-     
-    #[test]
-    #[ignore]
-    fn runs_with_strip() {
-        let mut cmd = Command::cargo_bin("text-splitter").unwrap();
-        let current_user = env::var("USER").unwrap();
+#[test]
+fn runs_nostrip() {
+    let mut cmd = Command::cargo_bin("text-splitter").unwrap();
+    cmd.args(["--input-files", "tests/inputs/files2process.txt", 
+            "--dir", "tests/inputs/", "-o", "tests/outputs/", 
+            "--minchar", "400", "--maxchar", "1200", "-v"]).assert().success();
+}
+    
+#[test]
+#[ignore]
+fn runs_with_strip() {
+    let mut cmd = Command::cargo_bin("text-splitter").unwrap();
+    let current_user = env::var("USER").unwrap();
 
-        cmd.args(["--input-files", &format!("/media/{}/ssd1/aiken/files2process.txt", current_user), 
-                   "--dir", &format!("/media/{}/ssd1/aiken/aiken-lang-docs/src/", current_user),
-                   "-o", &format!("/media/{}/ssd1/aiken/aiken-docs-processed/", current_user), 
-                   "--minchar", "400", "--maxchar", "1200",
-                   "--strip-prefix", &format!("/media/{}/ssd1/aiken/aiken-lang-docs/src/pages/", current_user),
-                   "--prefix-replace", "https://aiken-lang.org/"])
-                   .assert().success();
-
-    }
+    cmd.args(["--input-files", &format!("/media/{}/ssd1/aiken/files2process.txt", current_user), 
+                "--dir", &format!("/media/{}/ssd1/aiken/aiken-lang-docs/src/", current_user),
+                "-o", &format!("/media/{}/ssd1/aiken/aiken-docs-processed/", current_user), 
+                "--minchar", "400", "--maxchar", "1200",
+                "--strip-prefix", &format!("/media/{}/ssd1/aiken/aiken-lang-docs/src/pages/", current_user),
+                "--prefix-replace", "https://aiken-lang.org/"])
+                .assert().success();
 
 }
+
+
 
