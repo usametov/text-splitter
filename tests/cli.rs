b3660b0fcf01b8dc11ab91bef967ea55f843ceb7
usametov <usametov@gmail.com>
2024-03-14 23:14:07 -0400

    clean up

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,42 +1,44 @@
-use assert_cmd::Command; 
-use predicates::prelude::*;
-//use std::env;
+#[cfg(test)]
+mod tests {
+
+    use assert_cmd::Command; 
+    use predicates::prelude::*;
+    use std::env;
+
+    #[test]
+    fn dies_no_args() {
+
+        let mut cmd = Command::cargo_bin("text-splitter").unwrap();
+        cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("USAGE"));
+        
+    }
 
 
-#[test]
-fn dies_no_args() {
+    #[test]
+    fn runs_nostrip() {
+        let mut cmd = Command::cargo_bin("text-splitter").unwrap();
+        cmd.args(["--input-files", "tests/inputs/files2process.txt", 
+                "--dir", "tests/inputs/", "-o", "tests/outputs/", 
+                "--minchar", "400", "--maxchar", "1200", "-v"]).assert().success();
+    }
+     
+    #[test]
+    #[ignore]
+    fn runs_with_strip() {
+        let mut cmd = Command::cargo_bin("text-splitter").unwrap();
+        let current_user = env::var("USER").unwrap();
 
-    let mut cmd = Command::cargo_bin("text-splitter").unwrap();
-    cmd.assert()
-       .failure()
-       .stderr(predicate::str::contains("USAGE"));
-    
+        cmd.args(["--input-files", &format!("/media/{}/ssd1/aiken/files2process.txt", current_user), 
+                   "--dir", &format!("/media/{}/ssd1/aiken/aiken-lang-docs/src/", current_user),
+                   "-o", &format!("/media/{}/ssd1/aiken/aiken-docs-processed/", current_user), 
+                   "--minchar", "400", "--maxchar", "1200",
+                   "--strip-prefix", &format!("/media/{}/ssd1/aiken/aiken-lang-docs/src/pages/", current_user),
+                   "--prefix-replace", "https://aiken-lang.org/"])
+                   .assert().success();
+
+    }
+
 }
-
-
-#[test]
-fn runs_nostrip() {
-    let mut cmd = Command::cargo_bin("text-splitter").unwrap();
-    cmd.args(["--input-files", "tests/inputs/files2process.txt", 
-               "--dir", "tests/inputs/", "-o", "tests/outputs/", 
-               "--minchar", "400", "--maxchar", "1200", "-v"]).assert().success();
-}
-
-// uncomment this test, if you wan to transform src attribute
-// #[test]
-// fn runs_with_strip() {
-//     let mut cmd = Command::cargo_bin("text-splitter").unwrap();
-//     let current_user = env::var("USER").unwrap();
-
-//     cmd.args(["--input-files", &format!("/media/{}/ssd1/aiken/files2process.txt", current_user), 
-//                "--dir", &format!("/media/{}/ssd1/aiken/aiken-lang-docs/src/", current_user),
-//                "-o", &format!("/media/{}/ssd1/aiken/aiken-docs-processed/", current_user), 
-//                "--minchar", "400", "--maxchar", "1200",
-//                "--strip-prefix", &format!("/media/{}/ssd1/aiken/aiken-lang-docs/src/pages/", current_user),
-//                "--prefix-replace", "https://aiken-lang.org/"])
-//                .assert().success();
-
-// }
-
-
 
